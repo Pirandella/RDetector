6423b5dce446ed1372cc495f2a334c4ba17c1456
Sergey <dexxonone@gmail.com>
2020-05-07 20:20:50 +0300

    Fix: day roll out

--- src/main.c
+++ src/main.c
@@ -27,12 +27,15 @@ int main(int argc, char **argv){
     int aFibFlag = 0;
 
 
+#ifdef REJECT
     int gP = 0;
+#endif // REJECT
     int i = 0;
     float rr = 0;
     uint8_t firstR = 0;
     int aFibStartTime = 0;
     int aFibEndTime = 0;
+    uint8_t newDay = 0;
 #ifdef GRUBBS
     int rrIndex = 0;
     float rrIntervalBuffer[DATASET_SIZE];
@@ -84,10 +87,12 @@ int main(int argc, char **argv){
 
         aFibStartTime = (args->timeCode[i].sh * 3600) + (args->timeCode[i].sm * 60) + args->timeCode[i].ss;
         aFibEndTime = (args->timeCode[i].eh * 3600) + (args->timeCode[i].em * 60) + args->timeCode[i].es;
-        printf("\x1B[33mFirst time interval: %02d:%02d:%02d->%02d:%02d:%02d\x1B[0m\n", args->timeCode[i].sh, args->timeCode[i].sm, args->timeCode[i].ss, args->timeCode[i].eh, args->timeCode[i].em, args->timeCode[i].es);
+        printf("\x1B[33mFirst time interval: %02d:%02d:%02d->%02d:%02d:%02d\x1B[0m\n", args->timeCode[i].sh, args->timeCode[i].sm, args->timeCode[i].ss,
+                args->timeCode[i].eh, args->timeCode[i].em, args->timeCode[i].es);
         peak.value = 0.0;
         while(!feof(ecgFile)){
-            fscanf(ecgFile, "%d.%d.%d\t%d\t%d\t%f\t%f\t%f\t%f\n", &ecg->year, &ecg->month, &ecg->day, &ecg->hours, &ecg->minutes, &ecg->seconds, &ecg->ch0, &ecg->ch1, &ecg->ch2);
+            fscanf(ecgFile, "%d.%d.%d\t%d\t%d\t%f\t%f\t%f\t%f\n", &ecg->year, &ecg->month, &ecg->day, &ecg->hours,
+                    &ecg->minutes, &ecg->seconds, &ecg->ch0, &ecg->ch1, &ecg->ch2);
             switch(args->algorithm){
                 case 0:
                     qrs->lowPass = lowPassFilter(ecg->ch0);
@@ -118,21 +123,27 @@ int main(int argc, char **argv){
             if(peak.index > 0){
                 peak.value = ecg->ch0;
                 int fileTimeSec = (ecg->hours * 3600) + (ecg->minutes * 60) + (int)ecg->seconds;
+                if(fileTimeSec == 0) newDay = 1;
                 if(aFibFlag != 2){
-                    if((fileTimeSec >= aFibStartTime) && (fileTimeSec <= aFibEndTime)){
+                    if( ((fileTimeSec >= aFibStartTime) && (fileTimeSec <= aFibEndTime)) ||
+                        ((newDay) && (fileTimeSec <= aFibEndTime)) ||
+                        ((aFibStartTime > aFibEndTime) && (fileTimeSec >= aFibStartTime)) ){
                         printf("\b%c[2K%d:%d:%f\r", 27, ecg->hours, ecg->minutes, ecg->seconds);
                         fflush(stdout);
                         aFibFlag = 1;
                         aFibKnown = 1;
                     }else if((fileTimeSec > aFibEndTime) && (aFibFlag == 1)){
                         if(i != (args->nTime - 1)){
+                            printf("%d:%d:%f\n", ecg->hours, ecg->minutes, ecg->seconds);
                             ++i;
-                            printf("\x1B[33mNext time interval: %02d:%02d:%02d->%02d:%02d:%02d\x1B[0m\n", args->timeCode[i].sh, args->timeCode[i].sm, args->timeCode[i].ss, args->timeCode[i].eh, args->timeCode[i].em, args->timeCode[i].es);
+                            printf("\x1B[33mCurrent time interval: %02d:%02d:%02d->%02d:%02d:%02d\x1B[0m\n", args->timeCode[i].sh, args->timeCode[i].sm,
+                                    args->timeCode[i].ss, args->timeCode[i].eh, args->timeCode[i].em, args->timeCode[i].es);
                             aFibStartTime = (args->timeCode[i].sh * 3600) + (args->timeCode[i].sm * 60) + args->timeCode[i].ss;
                             aFibEndTime = (args->timeCode[i].eh * 3600) + (args->timeCode[i].em * 60) + args->timeCode[i].es;
                             aFibFlag = 0;
                             aFibKnown = 0;
                         }else{
+                            printf("%d:%d:%f\n", ecg->hours, ecg->minutes, ecg->seconds);
                             printf("\x1B[32mNo more time intervals\x1B[0m\n");
                             aFibFlag = 2;
                             aFibKnown = 0;
@@ -150,9 +161,7 @@ int main(int argc, char **argv){
 #endif // ADAPTIVE_DELAY
 #ifdef MOVING_AVERAGE
                 float v = movingAvg(&rr);
-                if(v != -1000){
-                    fprintf(rrFile, "%02d:%02d:%09.6f\t%f\t%d\n", ecg->hours, ecg->minutes, ecg->seconds, v, aFibKnown);
-                }
+                if(v != -1000) fprintf(rrFile, "%02d:%02d:%09.6f\t%f\t%d\n", ecg->hours, ecg->minutes, ecg->seconds, v, aFibKnown);
 #endif // MOVING_AVERAGE
 #ifdef GRUBBS
                 if(rrIndex == DATASET_SIZE){
@@ -160,12 +169,15 @@ int main(int argc, char **argv){
                     int g = grubbsReject(rrIntervalBuffer, rrTimeBuffer, DATASET_SIZE, 2);
                     // int g = outliner(rrIntervalBuffer, rrTimeBuffer, DATASET_SIZE);
                     for(int k = 0; k < g; k++)
-                            fprintf(rrFile, "%02d:%02d:%09.6f\t%f\t%d\n", rrTimeBuffer[k].h, rrTimeBuffer[k].m, rrTimeBuffer[k].s, rrIntervalBuffer[k], aFibKnown);
+                            fprintf(rrFile, "%02d:%02d:%09.6f\t%f\t%d\n", rrTimeBuffer[k].h, rrTimeBuffer[k].m, rrTimeBuffer[k].s,
+                                    rrIntervalBuffer[k], aFibKnown);
                     gP += g; // Used to track final number of RR intervals
 #endif // REJECT
 #ifdef LABEL
                     grubbsLable(rrIntervalBuffer, rrLabelBuffer, DATASET_SIZE);
-                    for(int k = 0; k < DATASET_SIZE; k++) fprintf(rrFile, "%02d:%02d:%09.6f\t%f\t%d\n", rrTimeBuffer[k].h, rrTimeBuffer[k].m, rrTimeBuffer[k].s, rrIntervalBuffer[k], (rrLabelBuffer[k] == -1) ? -1 : aFibKnown);
+                    for(int k = 0; k < DATASET_SIZE; k++)
+                        fprintf(rrFile, "%02d:%02d:%09.6f\t%f\t%d\n", rrTimeBuffer[k].h, rrTimeBuffer[k].m, rrTimeBuffer[k].s,
+                                rrIntervalBuffer[k], (rrLabelBuffer[k] == -1) ? -1 : aFibKnown);
 #endif //LABEL
                     rrIndex = 0;
                 }else{
@@ -180,13 +192,15 @@ int main(int argc, char **argv){
                 fprintf(rrFile, "%02d:%02d:%09.6f\t%f\t%d\n", ecg->hours, ecg->minutes, ecg->seconds, rr, aFibKnown);
 #endif // !GRUBBS && !MOVING_AVERAGE
             }
-            fprintf(qrsFile, "%02d:%02d:%09.6f\t%9d\t%4d\t%f\t%f\t%d\t%d\n", ecg->hours, ecg->minutes, ecg->seconds, globalIndex, peak.index ? ++peakCount : 0, peak.value, ecg->ch0, aFibKnown, aFibFound);
+            fprintf(qrsFile, "%02d:%02d:%09.6f\t%9d\t%4d\t%f\t%f\t%d\t%d\n", ecg->hours, ecg->minutes, ecg->seconds,
+                    globalIndex, peak.index ? ++peakCount : 0, peak.value, ecg->ch0, aFibKnown, aFibFound);
             globalIndex++;
             if(firstR) localIndex++;
         }
 
+#ifdef REJECT
         printf("Total number of RR intervals: %d\nTotal number of RR after filtering: %d\n", peakCount, gP);
-
+#endif // REJECT
         fclose(ecgFile);
         fclose(qrsFile);
         fclose(rrFile);
